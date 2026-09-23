e2f03e7141c1ddac94035855137dbc95f5c215b3
Cody Bergin <cbergin@vividsolutions.com>
2026-09-23 12:12:16 -0700

    WFWX-3004 Basic instance of SMK functional in map

--- frontend/src/app/components/station-selection/station-selection-map/station-selection-map.component.ts
+++ frontend/src/app/components/station-selection/station-selection-map/station-selection-map.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+declare const SMK: any;
 
 @Component({
-  selector: 'app-station-selection-map',
-  templateUrl: './station-selection-map.component.html',
-  styleUrl: './station-selection-map.component.scss'
+    selector: 'app-station-selection-map',
+    templateUrl: './station-selection-map.component.html',
+    styleUrl: './station-selection-map.component.scss'
 })
-export class StationSelectionMapComponent {}
+
+export class StationSelectionMapComponent implements AfterViewInit {
+
+    async ngAfterViewInit(): Promise<void> {
+
+        await SMK.INIT({
+            containerSel: '#station-map',
+            config: [
+                './assets/smk/station-selection-config.json',
+                '?'
+            ]
+        });
+
+    }
+
+}
