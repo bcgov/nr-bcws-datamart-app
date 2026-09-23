143152d064edd8b42971d71d47ceab967db8c0e8
Cody Bergin <cbergin@vividsolutions.com>
2026-09-23 16:34:22 -0700

    WFWX-3004 remove smk items we're not using, update mobile functionality of map and list container

--- frontend/src/app/components/station-selection/station-selection-content/station-selection-content.component.ts
+++ frontend/src/app/components/station-selection/station-selection-content/station-selection-content.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
-  selector: 'app-station-selection-content',
-  templateUrl: './station-selection-content.component.html',
-  styleUrl: './station-selection-content.component.scss'
+    selector: 'app-station-selection-content',
+    templateUrl: './station-selection-content.component.html',
+    styleUrl: './station-selection-content.component.scss'
 })
 export class StationSelectionContentComponent {
 
-  activeMobileTab: 'list' | 'map' = 'list';
+    activeMobileTab: 'list' | 'map' = 'list';
 
+    isMobile = false;
 
+    private readonly destroyRef = inject(DestroyRef);
+
+    constructor(
+        private readonly breakpointObserver: BreakpointObserver
+    ) {
+
+        this.breakpointObserver
+            .observe(['(max-width: 992px)'])
+            .pipe(
+                takeUntilDestroyed(this.destroyRef)
+            )
+            .subscribe(result => {
+
+                this.isMobile = result.matches;
+
+            });
+    }
 }
